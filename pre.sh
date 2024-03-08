2799ec8a0e59a61f1e9158b07066220b5defc7bc
indisposed <dev@indisposition.net>
2024-03-09 04:25:25 +1300

    a

--- pre.sh
+++ pre.sh
@@ -4,7 +4,8 @@ echo "we pregame"
 
 # if i want to account for other disk configs i will need to make this dynamic
 #
+
 curl https://raw.githubusercontent.com/dolevep/nixos/main/disko_btrfs.nix -o /tmp/disko.nix
 
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device '"/dev/nvme0n1"'
 
