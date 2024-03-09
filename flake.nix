5e7cf5a44133aeff4f2cf0d0de86d5e0e5ce2cad
indisposed <dev@indisposition.net>
2024-03-09 16:54:25 +1300

    a

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
 # @niceguy
 # Flakes is a feature of managing Nix packages to simplify usability and improve reproducibility of Nix installations. Flakes manages dependencies between Nix expressions, which are the primary protocols for specifying packages. Flakes implements these protocols in a consistent schema with a common set of policies for managing packages.
 #   https://nixos.wiki/wiki/Flakes
+#
+# flakes let you modulate and create seperate configs more easily
+# this was going to be the 'copycat' configuration but a default is required.
+# for now, this is the default
+# 
 {
   description = "NixOS flake config";
      
@@ -27,15 +32,15 @@
 
   outputs = {nixpkgs, ...} @ inputs:
   {
-    nixosConfigurations.copycat = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
-        inputs.disko.nixosModules.copycat
+        inputs.disko.nixosModules.default
         (import ./disko.nix { device = "/dev/nvme0n1"; })
 
         ./configuration.nix
               
-        # inputs.home-manager.nixosModules.default # default should be copycat
+        # inputs.home-manager.nixosModules.default 
         # inputs.impermanence.nixosModules.impermanence
       ];
     };
