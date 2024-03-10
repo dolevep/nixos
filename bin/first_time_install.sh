2fcb2fe10711cb6cbc522851452f1e6d204005c8
indisposed <dev@indisposition.net>
2024-03-10 20:05:42 +1300

    a

--- bin/first_time_install.sh
+++ bin/first_time_install.sh
@@ -35,9 +35,8 @@ esac
 
 curl https://raw.githubusercontent.com/dolevep/nixos/main/base/disko.nix?$RANDOM -o /tmp/disko.nix
 
-echo "fuck you"
-echo "fuck you"
-nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device \'"/dev/${DISK_DEV}"\'
+echo "fuck you for now no variable on this ... its being shitty"
+nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device \'"/dev/nvme0n1"\'
 echo "hug you"
 exit 1
 
