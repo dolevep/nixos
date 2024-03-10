f0f328da008751bc791245c31673ba421795cdd8
indisposed <dev@indisposition.net>
2024-03-10 20:00:00 +1300

    y

--- bin/first_time_install.sh
+++ bin/first_time_install.sh
@@ -35,8 +35,10 @@ esac
 
 curl https://raw.githubusercontent.com/dolevep/nixos/main/base/disko.nix?$RANDOM -o /tmp/disko.nix
 
-nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device "/dev/${DISK_DEV}"
 echo fuck you
+echo fuck you
+nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device "/dev/${DISK_DEV}"
+echo hug you
 exit 1
 
 
